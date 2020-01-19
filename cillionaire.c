d13ae8bdbedd46b0b52cec2a1fe7181ff965b2b5
André Furão Figueira <affigueir@gmail.com>
2020-01-19 20:50:21 +0000

    Fix text

--- cillionaire.c
+++ cillionaire.c
@@ -5,10 +5,9 @@
 #include <math.h>
 
 
-void print_menu(), print_credits();
-int joker();
-int seeded = 0, level = 1, j25 = 1, j50 = 1, run = 1;
-char command[32], name[32];
+void print_menu(), print_credits(), question_get();
+int seeded = 0, level = 1,  run = 1;
+char command[32], name[32], *filename, j25[] = "YES", j50[] = "YES";
 FILE *fpointer;
 
 int main(int argc, char **argv) {
@@ -24,7 +23,8 @@ int main(int argc, char **argv) {
 
         else if (argv[i][1] == 'f') {
 
-            fpointer = fopen(argv[i+1], "rw");
+            filename = argv[i + 1];
+            fpointer = fopen(filename, "rt");
 
         }
 
@@ -32,29 +32,47 @@ int main(int argc, char **argv) {
 
     if (!seeded) { srand(time(0)); }
 
-    print_menu();
+    puts("********************************************");
+    puts("***            CILLIONAIRE                 *");
+    puts("********************************************");
+    puts("*** n <name>     - new game                *");
+    puts("*** q            - quit                    *");
+    puts("*** h            - show this menu          *");
+    puts("*** r <filename> - resume game             *");
+    puts("*** s <filename> - save progress and quit  *");
+    puts("*** j 50         - play 50:50 joker        *");
+    puts("*** j 25         - play 25:75 joker        *");
+    puts("*** c            - show credits            *");
+    puts("********************************************");
 
     do {
-        gets(command);
+        fgets(command, 32, stdin);
 
         switch(command[0]) {
 
         case 'n':
             level = 1;
-            j25 = 1;
-            j50 = 1;
+            strcpy(j50, "YES");
+            strcpy(j25, "YES");
             if (command[1] == '\0') {
                 strcpy(name, "newbie");
                 }
             else {
-                for (int i = 2; command[i] != '\0'; i++) {
+                for (int i = 2; command[i] != '\n'; i++) {
                     name[i - 2] = command[i];
                 }
             }
+            printf("*** Hi %s, let's get started!\n", name);
+            puts("********************************************");
+            printf("*** Name:  %s                           *\n", name);
+            printf("*** Level: %d                               *\n", level);
+            printf("*** j50:   %s                             *\n", j50);
+            printf("*** j25:   %s                             *\n", j25);
+            puts("********************************************");
             break;
 
         case 'q':
-            printf("Sad to see you go...");
+            puts(">*** Sad to see you go...");
             run = 0;
             break;
 
@@ -64,13 +82,11 @@ int main(int argc, char **argv) {
 
         case 'j':
             if (strcmp(command, "j 25") == 0) {
-                joker();
-                j25 = 0;
+                strcpy(j25, "NO");
                 break;
             }
             else if (strcmp(command, "j 50") == 0) {
-                joker();
-                j50 = 0;
+                strcpy(j25, "NO");
                 break;
             }
             else {
@@ -94,28 +110,24 @@ int main(int argc, char **argv) {
 
 void print_menu() {
 
+    puts(">********************************************");
+    puts("***            CILLIONAIRE                 *");
     puts("********************************************");
-    puts("***             CILLIONAIRE                *");
-    puts("********************************************");
-    puts("*** n <name> - new game                    *");
-    puts("*** q - quit                               *");
-    puts("*** h - show this menu                     *");
+    puts("*** n <name>     - new game                *");
+    puts("*** q            - quit                    *");
+    puts("*** h            - show this menu          *");
     puts("*** r <filename> - resume game             *");
     puts("*** s <filename> - save progress and quit  *");
-    puts("*** j 50 - play 50:50 joker                *");
-    puts("*** j 25 - play 25:75 joker                *");
-    puts("*** c - show credits                       *");
+    puts("*** j 50         - play 50:50 joker        *");
+    puts("*** j 25         - play 25:75 joker        *");
+    puts("*** c            - show credits            *");
     puts("********************************************");
 
-}
-
-int joker() {
-    return rand() % 4;
 }
 
 void print_credits() {
     puts("**********************************");
-    puts("*** Trabalho realizado por...  ***");
+    puts("***           Autores          ***");
     puts("*** Andre Figueira - 21901435  ***");
     puts("*** Joao Matos - 21901219      ***");
     puts("*** Ruben Cerqueira - 21902961 ***");
